07aba544f6df449f10df4843970392e32c8892f7
BRENHINES <102440059+BRENHINES@users.noreply.github.com>
2024-02-01 02:30:13 -0500

    FEAT: Adding mechanics features in Angle Converter Function file

--- Day003-Units_Converter/Converter_Functions/Angle_Converter_Function.py
+++ Day003-Units_Converter/Converter_Functions/Angle_Converter_Function.py
@@ -1,6 +1,9 @@
-def degree_to_rad():
-    pass
+import math
 
 
-def rad_to_degree():
-    pass
+def degree_to_rad(degree_value):
+    return str(math.radians(degree_value)) + "π rad"
+
+
+def rad_to_degree(rad_value):
+    return str(math.degrees(rad_value)) + "°"
