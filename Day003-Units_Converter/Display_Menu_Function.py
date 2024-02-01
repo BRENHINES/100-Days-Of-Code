f29f8608665737ba3da6377eb35253f4c7e66c42
BRENHINES <102440059+BRENHINES@users.noreply.github.com>
2024-02-01 01:00:23 -0500

    FEAT: Adding our menu which must be display in the program and the files for the units converter of all the units

---
+++ Day003-Units_Converter/Display_Menu_Function.py
@@ -0,0 +1,85 @@
+"""
+All the menu which must be displayed
+"""
+def display_principal_menu():
+    print("🤗🤗 Salut ! Je suis là aujourd'hui pour t'aider a faire des conversion de manière efficace.\n")
+    print("Alors, Choisissons l'unité que vous avez")
+    print("1. Unité de longueur")
+    print("2. Unité de volume")
+    print("3. Unité de monnaie")
+    print("4. Unité de masse")
+    print("5. Unité de temps")
+    print("6. Unité de température")
+    print("7. Unité d' angle")
+
+
+def display_length_unit_menu():
+    print("Vous voulez convertir une longueur, choisissez les deux unités impliquées; sous la forme ")
+    print("unité à convertir , unité ou convertir . Exemple : 1 , 2 ")
+    print("1. Le mètre m")
+    print("2. Le kilometre km")
+    print("3. Le centimètre cm")
+    print("4. Le millimtre mm")
+    print("5. Le pied ft")
+    print("6. La yarde yd")
+    print("7. La ligue lg")
+
+
+def display_volume_unit_menu():
+    print("Vous voulez convertir un volume, choisissez les deux unités impliquées; sous la forme ")
+    print("unité à convertir , unité ou convertir . Exemple : 1 , 2 ")
+    print("1. Le mètre cube m^3")
+    print("2. Le litre l")
+    print("3. Le millilitre ml")
+    print("4. Le pied cube ft^3")
+    print("5. La pinte pt")
+    print("6. La gallon gal")
+
+
+def display_money_unit_menu():
+    print("Vous voulez convertir une monnaie, choisissez les deux unités impliquées; sous la forme ")
+    print("unité à convertir , unité ou convertir . Exemple : 1 , 1 ")
+    print("1. L'euro ")
+    print("2. Le dollar americain")
+    print("3. La livre sterling")
+    print("4. Le yen")
+    print("5. Le franc suisse")
+    print("6. Le franc CFA")
+
+
+def display_weight_unit_menu():
+    print("Vous voulez convertir un poids, choisissez les deux unités impliquées; sous la forme ")
+    print("unité à convertir , unité ou convertir . Exemple : 1 , 2 ")
+    print("1. Le gramme g")
+    print("2. Le kilogramme kg")
+    print("3. Le milligramme mg")
+    print("4. La tonne t")
+    print("5. La livre lb")
+    print("6. L'once oz")
+
+
+def display_time_unit_menu():
+    print("Vous voulez convertir une durée, choisissez les deux unités impliquées; sous la forme ")
+    print("unité à convertir , unité ou convertir . Exemple : 1 , 2 ")
+    print("1. La seconde s")
+    print("2. La minute m")
+    print("3. L'heure h")
+    print("4. Le jour j")
+    print("5. La semaine sem")
+    print("6. Le mois")
+    print("7. L'année")
+
+
+def display_hit_unit_menu():
+    print("Vous voulez convertir une température, choisissez les deux unités impliquées; sous la forme ")
+    print("unité à convertir , unité ou convertir . Exemple : 1 , 2 ")
+    print("1. Le celcius °C")
+    print("2. Le Fahrenheit °F")
+    print("3. Le Kelvin °K")
+
+
+def display_angle_unit_menu():
+    print("Vous voulez convertir un angle, choisissez les deux unités impliquées; sous la forme ")
+    print("unité à convertir , unité ou convertir . Exemple : 1 , 2 ")
+    print("1. Le degré °")
+    print("2. Le radian rad")
---
+++ Day003-Units_Converter/main.py
@@ -0,0 +1,9 @@
+"""
+Title : Units Converter
+
+Objectifs : Create a console application for units convertion
+Output: We will have a programme which ask to the user the unit he have and the unit he want to migrate
+        So ask for the input.
+"""
+
+def
