7b1ae495ef3140bcca8714b8d810511ea2769ff4
BRENHINES <102440059+BRENHINES@users.noreply.github.com>
2024-02-02 05:47:32 -0500

    TEST: Adding units test

---
+++ Day003-Units_Converter/UnitTests/Test_Angle_Converter_Function.py
@@ -0,0 +1,10 @@
+import unittest
+from . import Day003-Units_Converter.Converter_Functions
+
+class MyTestCase(unittest.TestCase):
+    def test_degree_to_rad(self):
+        self.assertEqual(True, True)  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
