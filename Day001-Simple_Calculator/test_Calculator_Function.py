ee09c294841c9f613c28a57b5357178bed23d5b2
BRENHINES <102440059+BRENHINES@users.noreply.github.com>
2024-02-04 02:01:57 -0500

    DOC: Adding the stub file for the Angle Converter Functions

--- Day001-Simple_Calculator/test_Calculator_Function.py
+++ Day001-Simple_Calculator/test_Calculator_Function.py
@@ -1,3 +1,25 @@
-import pytest
+import Calculator_Function as cf
 
 
+def test_addition():
+    assert cf.addition(1, 2) == 3
+
+
+def test_substraction():
+    assert cf.substraction(2, 1) == 1
+
+
+def test_divide():
+    assert cf.divide(1, 2) == 0.5
+
+
+def test_multiply():
+    assert cf.multiply(1, 1) == 1
+
+
+def test_integer_divide():
+    assert cf.integer_divide(3, 2) == 1
+
+
+def test_power():
+    assert cf.power(2, 3) == 8
--- Day003-Units_Converter/test_Converter_Functions/test_Angle_Converter_Function.py
+++ Day003-Units_Converter/test_Converter_Functions/test_Angle_Converter_Function.py
@@ -1,11 +1,3 @@
-import unittest
 from ..Converter_Functions import Angle_Converter_Function as af
 
 
-class MyTestCase(unittest.TestCase):
-    def test_degree_to_rad(self):
-        self.assertEqual(True, True)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
