22833e5f6d62ff2cea85a030c16c7dfceed871d4
BRENHINES <102440059+BRENHINES@users.noreply.github.com>
2024-02-05 17:46:15 -0500

    DOC: Adding the Money Converter Function Interfaces

---
+++ Day003-Units_Converter/Interfaces/Money_Converter_Function.pyi
@@ -0,0 +1,108 @@
+def euro_to_dollar(amount: float) -> float:
+    """euro to dollar
+
+    Helps us to convert to dollar an euro amount.
+
+    :param amount: the amount or the price we have to convert.
+    :return: the dollar amount.
+    """
+    ...
+
+
+def euro_to_livre(amount: float) -> float:
+    """euro to livre
+
+    Helps us to convert to livre an euro amount.
+
+    :param amount: the amount or the price we have to convert.
+    :return: the livre amount.
+    """
+    ...
+
+
+def euro_to_yen(amount: float) -> float:
+    """euro to yen
+
+    Helps us to convert to yen an euro amount.
+
+    :param amount: the amount or the price we have to convert.
+    :return: the yen amount.
+    """
+    ...
+
+
+def euro_to_CHF(amount: float) -> float:
+    """euro to CHF
+
+    Helps us to convert to CHF an euro amount.
+
+    :param amount: the amount or the price we have to convert.
+    :return: the CHF amount.
+    """
+    ...
+
+
+def euro_to_CFA(amount: float) -> float:
+    """euro to CFA
+
+    Helps us to convert to CFA a euro amount.
+
+    :param amount: the amount or the price we have to convert.
+    :return: the CFA amount.
+    """
+    ...
+
+
+def dollar_to_euro(amount: float) -> float:
+    """dollar to euro
+
+    Helps us to convert to euro a dollar amount.
+
+    :param amount: the amount or the price we have to convert.
+    :return: the euro amount.
+    """
+    ...
+
+
+def livre_to_euro():
+    """livre to euro
+
+    Helps us to convert to euro a livre amount.
+
+    :param amount: the amount or the price we have to convert.
+    :return: the euro amount.
+    """
+    ...
+
+
+def yen_to_euro(amount: float) -> float:
+    """yen to euro
+
+    Helps us to convert to euro a yen amount.
+
+    :param amount: the amount or the price we have to convert.
+    :return: the euro amount.
+    """
+    ...
+
+
+def CHF_to_euro(amount: float) -> float:
+    """CHF to euro
+
+    Helps us to convert to euro a CHF amount.
+
+    :param amount: the amount or the price we have to convert.
+    :return: the euro amount.
+    """
+    ...
+
+
+def CFA_to_euro(amount: float) -> float:
+    """CFA to euro
+
+    Helps us to convert to euro a CFA amount.
+
+    :param amount: the amount or the price we have to convert.
+    :return: the euro amount.
+    """
+    ...
