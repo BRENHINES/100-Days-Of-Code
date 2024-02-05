72c3053d0aec992b46355372c168bd504cdc27dd
BRENHINES <102440059+BRENHINES@users.noreply.github.com>
2024-02-05 17:07:59 -0500

    DOC: Adding the Length Converter Functions interfaces for all the functions concerned

---
+++ Day003-Units_Converter/Interfaces/Lenght_Converter_Function.pyi
@@ -0,0 +1,130 @@
+def km_to_m(length: float) -> float:
+    """km to m
+
+    Helps us to convert any kilometer length into meter one corresponding.
+
+    :param length: the length which you want to convert.
+    :return: the meter corresponding of the length you entered
+    """
+    ...
+
+
+def m_to_cm(length: float) -> float:
+    """m to cm
+
+    Helps us to convert any meter length into centimeter one corresponding.
+
+    :param length: the length which you want to convert.
+    :return: the centimeter corresponding
+    """
+    ...
+
+
+def m_to_mm(length: float) -> float:
+    """m to mm
+
+    Helps us to convert any meter length into millimeter one corresponding.
+
+    :param length: the length which you want to convert.
+    :return: the millimeter corresponding
+    """
+    ...
+
+
+def ft_to_m(length: float) -> float:
+    """ft to m
+
+    Helps us to convert any feet length into meter one corresponding.
+
+    :param length: the length which you want to convert.
+    :return: the meter corresponding
+    """
+    ...
+
+
+def yd_to_m(length: float) -> float:
+    """yd to m
+
+    Helps us to convert any yard length into meter one corresponding.
+
+    :param length: the length which you want to convert.
+    :return: the meter corresponding
+    """
+    ...
+
+
+def league_to_km(length: float) -> float:
+    """league to km
+
+    Helps us to convert any league length into kilometer one corresponding.
+
+    :param length: the length which you want to convert.
+    :return: the kilometer corresponding
+    """
+    ...
+
+
+def m_to_km(length: float) -> float:
+    """m to km
+
+    Helps us to convert any meter length into kilometer one corresponding.
+
+    :param length: the length which you want to convert.
+    :return: the kilometer corresponding
+    """
+    ...
+
+
+def cm_to_m(length: float) -> float:
+    """cm to m
+
+    Helps us to convert any centimeter length into meter one corresponding.
+
+    :param length: the length which you want to convert.
+    :return: the meter corresponding
+    """
+    ...
+
+
+def mm_to_m(length: float) -> float:
+    """mm to m
+
+    Helps us to convert any millimeter length into meter one corresponding.
+
+    :param length: the length which you want to convert.
+    :return: the meter corresponding
+    """
+    ...
+
+
+def m_to_ft(length: float) -> float:
+    """m to ft
+
+    Helps us to convert any meter length into feet one corresponding.
+
+    :param length: the length which you want to convert.
+    :return: the feet corresponding
+    """
+    ...
+
+
+def m_to_yd(length: float) -> float:
+    """m to yd
+
+    Helps us to convert any meter length into yard one corresponding.
+
+    :param length: the length which you want to convert.
+    :return: the yard corresponding
+    """
+    ...
+
+
+def km_to_league(length: float) -> float:
+    """km to league
+
+    Helps us to convert any kilometer length into league one corresponding.
+
+    :param length: the length which you want to convert.
+    :return: the league corresponding
+    """
+    ...
